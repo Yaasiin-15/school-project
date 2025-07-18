170d78e5b317add4029e36bec1db8cdca87752f4
Yaasiin Mohamuud <wllhero145@gmail.com>
2025-07-19 00:59:56 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ import logger from '../utils/logger.js';
 
 const connectDatabase = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://school_management_system:Yaasiin%402026@cluster0.fvacpn1.mongodb.net/school_management?retryWrites=true&w=majority&appName=Cluster0';
+    const mongoURI = process.env.MONGODB_URI;
     
     const options = {
       // Connection options for production
